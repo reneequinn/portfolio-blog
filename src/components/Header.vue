07a1b24c889ab875748b24b399cc946a9f714b08
Renee Quinn <reneequinn17@gmail.com>
2020-09-16 15:19:31 +0930

    Adds beginner base styling and header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div class="container">
+      <h1>Renee Quinn</h1>
+      <nav>
+        <ul>
+          <li>
+            <g-link to="/">Home</g-link>
+          </li>
+          <li>
+            <g-link to="/about">About</g-link>
+          </li>
+          <li>
+            <g-link to="/projects">Projects</g-link>
+          </li>
+          <li>
+            <g-link to="/blog">Blog</g-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
+    <Header />
     <slot />
   </div>
 </template>
+
+<script>
+import Header from '~/components/Header';
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 
 <static-query>
 query {
